0b874b9257f84007c7f604f6888f5a951ecf254c
Marissa Zhong <mwz2101@columbia.edu>
2018-08-06 22:12:46 +0000

    automatically backed up by learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -5,6 +5,11 @@ export default DelayedButton extends React.Component {
 
   this.props.onDelayedClick
   this.props.delay
-  
+
+  render() {
+    return(
+      <button handleClick={this.handleClick} />
+    )
+  }
 
 }
